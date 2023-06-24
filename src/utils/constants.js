433150134dea9e97eab38b607932806697b4854f
Abhay <abhaysharma.developer@gmail.com>
2023-06-24 22:19:06 +0530

    Final Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Latest', icon: <HomeIcon />, },
+  { name: 'Youtube', icon: <YouTubeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+];
